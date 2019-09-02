36792ef86a79314431b0ee37664d1ba8c6d2d0a5
bautistaib <36866639+bautistaib@users.noreply.github.com>
2019-09-02 19:37:39 -0300

    Update analisis_LEED.py

--- analisis_LEED.py
+++ analisis_LEED.py
@@ -9,12 +9,26 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+import math
 from scipy import signal
 from scipy.signal import find_peaks
 csvs=[]
 xxx=[]
 yyy=[]
 import os
+
+def find_nearest(array,value):
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        return array[idx-1]
+    else:
+        return array[idx]
+
+#def find_nearest(array, value):
+#    array = np.asarray(array)
+#    idx = (np.abs(array - value)).argmin()
+#    return array[idx]
+
 for root2, dirs2, files2 in os.walk("C:/Users/Bautista/Downloads/Fotos camara oscar 4.5 a 50.5 en jpg/text"):
     for file in files2:
         if file.endswith(".csv"):
@@ -72,19 +86,42 @@ for file in csvs:
             if(tmp2.isdigit()):
                 xxx.append(float(tmp2)+2.5)
                 yyy.append(list(picos_processed))
-        xxx_processed = np.sqrt(xxx)
+xxx_processed = 1./np.sqrt(xxx)
 xxxx=np.array(xxx_processed)
-yyyy=np.array(yyy)
-
-
-
+#yyyy=np.array(yyy)
+primer_orden=np.zeros(len(xxx))
+for i in range(len(yyy)):
+    primer_orden[i]=find_nearest(yyy[i],1.23*xxxx[i]+0.068 )
+    print(yyy[i], xxxx[i], 1.23*xxxx[i]+0.068, primer_orden[i] )
+    
+#pendiente 0.107
+#origen 0.32
+#for i in range(len(yyy)):
+#    if(len(yyy)-1-i<4 or len(yyy)-1-i==19):
+#        #print(yyy[i])
+#        primer_orden.append(yyy[i][-1])
+#    else:
+#        if(i!=0):
+#            #print(yyy[i])
+#            primer_orden.append(yyy[i][-2])
+#        else:
+#            print(yyy[i])
+#            primer_orden.append(yyy[i][-3])
+#primer_orden.reverse()
+    
+yyyy=np.array(primer_orden)
 plt.clf()
-for xe, ye in zip(xxxx, yyyy):
-    plt.scatter([xe] * len(ye), ye)
-#plt.xlim(0,30)
-#plt.plot(xxxx,yyyy[:0], label='Loaded from file!')
+#for xe, ye in zip(xxxx, yyyy):
+#    plt.scatter([xe] * len(ye), ye)
+plt.scatter(xxxx,yyyy)
+f=open('primer orden.dat', 'w')
+for i in range(len(xxxx)):
+    f.write("%f\t%f\n" %(xxxx[i], yyyy[i]))
+#plt.plot(xxxx,yyyy, label='Loaded from file!', style='x')
 plt.xlabel('Energia')
 plt.ylabel('y')
 plt.title('Interesting Graph/nCheck it out')
 plt.legend()
+plt.savefig('picos.jpeg')
 plt.show()
+f.close()
